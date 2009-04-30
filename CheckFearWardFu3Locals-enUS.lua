8cf8a25f3d5abfe55e9abd6235fa558ad8d6814c
Erik L. Vonderscheer <evonder@gmail.com>
2009-04-30 20:10:54 +0000

    FuBar_CheckFearWardFu: Local keyword substitution

--- CheckFearWardFu3Locals-enUS.lua
+++ CheckFearWardFu3Locals-enUS.lua
@@ -9,22 +9,7 @@ debug = true
 --@end-debug@
 
 local L = Rock("LibRockLocale-1.0"):GetTranslationNamespace("FuBar_CheckFearWardFu3")
-L:AddTranslations("enUS", function() return {
-	["SHT"] = "Show high time",
-	["SHTD"] = "Show high time in FuBar text",
-	["SLT"] = "Show low time",
-	["SLTD"] = "Show low time in FuBar text",
-	["debug"] = "Debugging",
-	["debugD"] = "Enable Debugging",
-	["SN"] = "Status Notification",
-	["SND"] = "Where to show Fear Wards status notifications",
-	["DCF"] = "Warn in Chat Window",
-	["DCFD"] = "Warn in Blizzard default chat window",
-	["CTRA"] = "Warn in CT_RA Style",
-	["CTRAD"] = "Warn in CT_RA raid warning style",
-	["AUD"] = "Audible Warning",
-	["AUDD"] = "Enable Audible Warning on loss of Fear Ward",
-	["BUFF"] = "No Fear Wards",
-} end)
+L:AddTranslations("enUS", function() return 
+--@localization(locale="enUS", format="lua_table", same-key-is-true=true, handle-subnamespaces="concat", namespace-delimiter=",")@
+ end)
 
---@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
